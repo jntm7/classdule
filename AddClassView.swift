3097e9d4a873683b0bbd3ae6095a82ec0ee91464
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-09-13 01:11:00 -0600

    Add class and item views

---
+++ AddClassView.swift
@@ -0,0 +1,18 @@
+//
+//  AddClassView.swift
+//  eduTable
+//
+//  Created by Jonathan Tam on 2025-09-13.
+//
+
+import SwiftUI
+
+struct AddClassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddClassView()
+}
---
+++ ClassItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ClassItemView.swift
+//  eduTable
+//
+//  Created by Jonathan Tam on 2025-09-13.
+//
+
+import SwiftUI
+
+struct ClassItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClassItemView()
+}
