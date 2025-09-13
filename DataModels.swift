0534e80c9464a414328e0b2c80f146f565893708
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-09-13 01:06:00 -0600

    Update DataModels.swift

--- DataModels.swift
+++ DataModels.swift
@@ -19,4 +19,56 @@ struct ClassSchedule: Identifiable {
     var startHour: Int
     var startMinute: Int
     var durationMinutes: Int
+    
+    var timeDisplay: String {
+        let startTime = formatTime(hour: startHour, minute: startMinute)
+        let endTime = calculateEndTime()
+        
+        return "\(startTime) - \(endTime)"
+    }
+    
+    // Calculate Total Minutes
+    var minutesFromStartOfDay: Int {
+        return (startHour - 6) * 60 + startMinute
+    }
+    
+    // Calculate Duration
+    private func calculateEndTime() -> String {
+        let totalStartMinutes = startHour * 60 + startMinute
+        let totalEndMinutes = totalStartMinutes + durationMinutes
+        let endHour = totalEndMinutes / 60
+        let endMinute = totalEndMinutes % 60
+        
+        return formatTime(hour: endHour, minute: endMinute)
+    }
+    
+    // Format Time for Display
+    private func formatTime(hour: Int, minute: Int) -> String {
+        let displayHour: Int
+        let timeOfDay: String
+        
+        // Handle AM/PM
+        if hour == 0 {
+            displayHour = 12
+            timeOfDay = "AM"
+        } else if hour < 12 {
+            displayHour = hour
+            timeOfDay = "AM"
+        } else if hour == 12 {
+            displayHour = 12
+            timeOfDay = "PM"
+        } else {
+            displayHour = hour - 12
+            timeOfDay = "PM"
+        }
+        
+        // Display
+        if minute == 0 {
+            return "\(displayHour) \(timeOfDay)"
+        } else {
+            return "\(displayHour):\(String(format: "%02d", minute))\(timeOfDay)"
+        }
+        
+    }
+    
 }
