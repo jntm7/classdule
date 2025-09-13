28060ca3b4deec9ae4cc80c6dda5e88ad649c209
Jonathan <108718802+jntm7@users.noreply.github.com>
2025-09-13 02:48:13 -0600

    Update ClassItemView.swift

--- ClassItemView.swift
+++ ClassItemView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ClassItemView: View {
+    let classItem: ClassSchedule
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 2) {
+            Text(classItem.title)
+                .font(.caption)
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+                .lineLimit(2)
+            
+            Text(classItem.timeDisplay)
+                .font(.caption2)
+                .foregroundColor(.white.opacity(0.9))
+        }
+        .padding(6)
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(classItem.color)
+        .cornerRadius(6)
+        .shadow(color: .black.opacity(0.1), radius: 2, x: 0, y: 1)
     }
 }
 
 #Preview {
-    ClassItemView()
+    ClassItemView(classItem: ClassSchedule(
+        title: "Math 101",
+        dayOfWeek: 1,
+        startHour: 8,
+        startMinute: 30,
+        durationMinutes: 90,
+        color: .blue
+    ))
+    .frame(width: 100)
+    .padding()
 }
